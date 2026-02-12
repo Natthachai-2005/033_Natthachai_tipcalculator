833c814f1ce1b655ec4491e3d5e3eeaa608ad096
Natthachai-2005 <6740011033@psu.ac.th>
2026-02-12 18:13:00 +0700

    update project

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/033_Natthachai_tipcalculator",
-  assetPrefix: "/033_Natthachai_tipcalculator/",
+  assetPrefix: "/033_Natthachai_tipcalculator",
 };
 
 export default nextConfig;
